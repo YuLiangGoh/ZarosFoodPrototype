3bd1b9f0e642a842df88049f1f61dd1989e058d5
Zachary <zach.goh@appxtream.com>
2022-08-14 17:18:29 +0800

    [Update] Resize image smaller

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:zaros_prototype/app/app_navigator.dart';
 import 'package:zaros_prototype/app/app_route.dart';
-import 'package:zaros_prototype/const/image_path.dart';
 import 'package:zaros_prototype/screens/splash/splash_screen_page.dart';
 
 void main() {
